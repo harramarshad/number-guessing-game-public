d59f5e7923c145d87b562a28ec453ed17c483317
(.-.)Harram <166056157+harramarshad@users.noreply.github.com>
2024-06-20 23:46:29 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
+import inquirer from "inquirer";
+  
 
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"userGuessNumber",
+        type:"number",
+        message:"Enter your guess number( Number limit from 1 to 5)"
+    },
+]);
+
+if(answer.userGuessNumber===randomNumber){
+    console.log("Congratulations! you guess a correct number")
+}else{
+    console.log("Please try again as you guess a wrong number")
+};
